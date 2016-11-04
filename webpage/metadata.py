3785124b0cdbdeba1a8cbaf9d3e9f5182a7f90b3
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2016-11-04 13:39:11 +0100

    initial commit

---
+++ webpage/metadata.py
@@ -0,0 +1,11 @@
+# this files contains basic metadata about the project. This data will be used
+# (by default) in the base.html and index.html
+
+PROJECT_METADATA = {
+    'title': 'VHIOE',
+    'author': 'Peter Andorfer',
+    'subtitle': 'Verwaltung und Herrschaft in Österreich',
+    'description': 'Project in cooperation with Thomas Stockinger (Uni Wien) to collect, standardize and analyze entries in "Geschäftsprotokolle", created by austrian mid-level (Bezirk) administratives.',
+    'github': 'https://github.com/acdh-oeaw/vhioe',
+    'purpose_de': 'Das Ziel des Projekts "VHIOE" ist die Publikation von Daten über VHIOE.',
+    'purpose_en': 'The purpose of the project "VHIOE" is the publication of research data concerning "VHIOE".'}
