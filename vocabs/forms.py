067971dade4a259dff67fbbc3d04101f7de3f968
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2016-11-09 15:25:05 +0100

    add upload script and view for SKOS files (RDF/XML) https://redmine.acdh.oeaw.ac.at/issues/7230

--- vocabs/forms.py
+++ vocabs/forms.py
@@ -5,6 +5,19 @@ from crispy_forms.layout import Submit
 from .models import SkosConcept, SkosConceptScheme, SkosLabel
 
 
+class UploadFileForm(forms.Form):
+    file = forms.FileField()
+
+    def __init__(self, *args, **kwargs):
+        super(UploadFileForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_tag = True
+        self.helper.form_class = 'form-horizontal'
+        self.helper.label_class = 'col-md-3'
+        self.helper.field_class = 'col-md-9'
+        self.helper.add_input(Submit('submit', 'import'),)
+
+
 class SkosConceptForm(forms.ModelForm):
     class Meta:
         model = SkosConcept
--- vocabs/urls.py
+++ vocabs/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 from . import views
+from . import import_views
 from . import dal_views
 from .models import SkosLabel, SkosConcept, SkosConceptScheme
 
 urlpatterns = [
     url(r'^$', views.SkosConceptListView.as_view(), name='skosconcept_list'),
+    url(r'^import/$', import_views.import_skos, name='skosconcept_import'),
     url(r'^(?P<pk>[0-9]+)$', views.SkosConceptDetailView.as_view(), name='skosconcept_detail'),
     url(r'^create/$', views.SkosConceptCreate.as_view(), name='skosconcept_create'),
     url(r'^update/(?P<pk>[0-9]+)$', views.SkosConceptUpdate.as_view(), name='skosconcept_update'),
